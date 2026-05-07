d496d3cff10faf7730c591129142fae9d3be0bc4
DAbhijay <d.abhijay0808@gmail.com>
2026-05-07 23:11:52 +0530

    Redis

--- backend/src/config/redis.js
+++ backend/src/config/redis.js
@@ -3,6 +3,8 @@ import Redis from 'ioredis';
 export const redis = new Redis({
   host: process.env.REDIS_HOST || 'redis',
   port: Number(process.env.REDIS_PORT || 6379),
+  password: process.env.REDIS_PASSWORD,
+  tls: process.env.REDIS_TLS === 'true' ? {} : undefined,
   maxRetriesPerRequest: null
 });
 
