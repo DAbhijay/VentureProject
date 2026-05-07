4d832c40d66a8c43fc9ab6fecf8af99d9e60f060
DAbhijay <d.abhijay0808@gmail.com>
2026-05-07 23:31:10 +0530

    Revert changes

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
-const client = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL + '/api'
-});
+const client = axios.create({ baseURL: import.meta.env.VITE_API_BASE_URL });
 
 client.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
